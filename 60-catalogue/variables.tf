6bd836ca82ffb0906c9e036e8c3f3dae1d438163
kvk55247 <kurapothulavamsi@gmail.com>
2025-10-19 19:53:09 +0530

    terraform

--- 60-catalogue/variables.tf
+++ 60-catalogue/variables.tf
@@ -8,3 +8,16 @@ variable "environment" {
     default = "dev"
 }
 
+
+
+variable "zone_id" {
+    default = "Z02592383JVQTDY6U9ADB"
+}
+
+
+
+variable "zone_name" {
+    default = "daws84s.info"
+  
+}
+
